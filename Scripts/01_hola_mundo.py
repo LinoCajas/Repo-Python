5dfd5bbdbe882a38f7c2499552eff80dfca39465
LinoCajas <ryu1_flax@yahoo.es>
2022-03-10 19:50:36 -0500

    Update 01_hola_mundo.py

--- Scripts/01_hola_mundo.py
+++ Scripts/01_hola_mundo.py
@@ -14,3 +14,10 @@ print(len(miLista))
 # sort solo sirve para numeros o letras pero no una mezcla
 miLista.sort()
 print(miLista)
+v1="A"
+v2="b"
+v3="c"
+#el operador + concatena pero la , separa
+print(v1+v2+v3)
+print(v1,v2,v3)
+print("asdasd",a)
